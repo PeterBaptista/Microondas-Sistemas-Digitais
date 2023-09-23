230db6ba7c2423b07e0d40de53fb849d992b2dd8
Pedro Baptista <pedrolk2012@gmail.com>
2023-09-23 17:58:55 -0300

    tested counter_0to7_non_recycled module

---
+++ counter_0to7/counter_0to7_non_recycled.v
@@ -0,0 +1,17 @@
+module counter_0to7_non_recycled (
+    input wire clk,       // Sinal de clock de entrada
+    output wire [2:0] out // Sinal de saída de 3 bits
+);
+
+    reg [2:0] count = 3'b0;  // Registrador de contagem de 3 bits
+
+    always @(posedge clk) begin
+        if (count == 3'b111) // Verifica se a contagem atingiu 7 (binário: 111)
+            count <= 3'b000; // Reinicia a contagem para 0 (binário: 000)
+        else
+            count <= count + 1; // Incrementa a contagem
+    end
+
+    assign out = count; // Saída é igual à contagem atual
+
+endmodule
