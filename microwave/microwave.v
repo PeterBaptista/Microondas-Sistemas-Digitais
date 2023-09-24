d5e45e2ea0b74da1c773e8ed8b190f368a150521
Pedro Baptista <pedrolk2012@gmail.com>
2023-09-24 19:45:03 -0300

    added microwave module

---
+++ microwave/microwave.v
@@ -0,0 +1,14 @@
+module microwave(
+    input wire start,
+    input wire stopn,
+    input wire clearn,
+    input wire door_closed,
+    input wire [9:0] keypad,
+    input wire clk,
+    output wire [6:0] second_units_display,
+    output wire [6:0] second_tens_display,
+    output wire [6:0] minutes_display
+)
+
+
+endmodule
